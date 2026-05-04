cf12628cd09423a7614f0d35ebf75c312a83c95d
Funsho <oluwafunshoalabi97@gmail.com>
2026-05-04 17:22:20 +0100

    ready for deploy

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ mongoose
   .connect(MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
